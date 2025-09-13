0abf8aa025aa193cc32efe35d8e3dec29aebe7fd
Daksh Tandel <167323670+dakshtandel-9@users.noreply.github.com>
2025-09-13 09:26:28 +0530

    13 sep first update

--- app/data/blazerImages.js
+++ app/data/blazerImages.js
@@ -2,6 +2,7 @@
 // Add new image URLs here and they will be automatically used in both ProductListing and ProductPage components
 
 export const blazerImages = [
+    'https://res.cloudinary.com/dlk0wvka6/image/upload/v1757598313/0148_xhzwbq.jpg',
 'https://res.cloudinary.com/dlk0wvka6/image/upload/v1757598313/0148_xhzwbq.jpg',
 'https://res.cloudinary.com/dlk0wvka6/image/upload/v1757598305/0161_my2nhb.jpg',
 'https://res.cloudinary.com/dlk0wvka6/image/upload/v1757598305/0193_nx5noa.jpg',
